1442babcfc94a21db9fcaf5d26f717fae4c1c85f
Taogya <132679709+taogya@users.noreply.github.com>
2025-05-29 23:36:41 +0900

    modify debug.py

--- pcb/debug.py
+++ pcb/debug.py
@@ -9,19 +9,19 @@ import subprocess
 import csv
 
 
-def edit_json_file(file,
-                   board_offset__top=1,
-                   board_offset__bottom=1,
-                   board_offset__left=1,
-                   board_offset__right=1,
-                   included_pads=[],
-                   excluded_pads=[],
-                   hidden_pads__designated_net=True,
-                   hidden_pads__other_nets=True,
-                   neighbouring_nets__in_use=True,
-                   neighbouring_nets__offset=0.01,
-                   neighbouring_nets__common_points=100,
-                   neighbouring_nets__netlist=[]):
+def edit_net_config(file,
+                    board_offset__top=1,
+                    board_offset__bottom=1,
+                    board_offset__left=1,
+                    board_offset__right=1,
+                    included_pads=[],
+                    excluded_pads=[],
+                    hidden_pads__designated_net=True,
+                    hidden_pads__other_nets=True,
+                    neighbouring_nets__in_use=True,
+                    neighbouring_nets__offset=0.01,
+                    neighbouring_nets__common_points=100,
+                    neighbouring_nets__netlist=[]):
     with open(file, "r") as fp:
         data = json.load(fp)
 
@@ -46,7 +46,44 @@ def edit_json_file(file,
     with open(file, "w") as fp:
         json.dump(data, fp, indent=4)
 
-# メイン処理
+
+def edit_simulation_settings(file,
+                             frequency__start=200e6,
+                             frequency__stop=4e9,
+                             pixel_size=5,
+                             max_steps=5e5,
+                             via__filling_epsilon=1,
+                             via__plating_thickness=50,
+                             mesh__xy=50,
+                             mesh__inter_layers=4,
+                             mesh__margin__xy=200,
+                             mesh__margin__z=200,
+                             margin__xy=500,
+                             margin__z=500):
+    with open(file, "r") as fp:
+        data = json.load(fp)
+        # 周波数設定
+        data["frequency"]["start"] = frequency__start
+        data["frequency"]["stop"] = frequency__stop
+        # ピクセルサイズ設定
+        data["pixel_size"] = pixel_size
+        # 最大ステップ数設定
+        data["max_steps"] = max_steps
+        # ビアの設定
+        data["via"]["filling_epsilon"] = via__filling_epsilon
+        data["via"]["plating_thickness"] = via__plating_thickness
+        # メッシュの設定
+        data["mesh"]["xy"] = mesh__xy
+        data["mesh"]["inter_layers"] = mesh__inter_layers
+        data["mesh"]["margin"]["xy"] = mesh__margin__xy
+        data["mesh"]["margin"]["z"] = mesh__margin__z
+        # マージンの設定
+        data["margin"]["xy"] = margin__xy
+        data["margin"]["z"] = margin__z
+
+    # 編集した内容をファイルに書き戻す
+    with open(file, "w") as fp:
+        json.dump(data, fp, indent=4)
 
 
 def main():
@@ -65,52 +102,83 @@ def main():
     # 各ネット設定ファイルごとに処理
     for file in glob.glob("net_configs/*.json"):
         included_pads = included_pads_dict.get(os.path.basename(file), [])
+
         # JSONファイル編集
-        edit_json_file(file,
-                       board_offset__top=50,
-                       board_offset__bottom=50,
-                       board_offset__left=50,
-                       board_offset__right=50,
-                       included_pads=included_pads,
-                       hidden_pads__designated_net=False,
-                       hidden_pads__other_nets=False,
-                       neighbouring_nets__in_use=False)
+        edit_net_config(file,
+                        board_offset__top=50,
+                        board_offset__bottom=50,
+                        board_offset__left=50,
+                        board_offset__right=50,
+                        included_pads=included_pads,
+                        hidden_pads__designated_net=False,
+                        hidden_pads__other_nets=False,
+                        neighbouring_nets__in_use=False)
+
         # スライス生成
         slice_main(file)
+
         # gerber生成コマンド実行
         subprocess.run(
             args=["kmake", "gerber", "-xe"],
             cwd=os.path.join("slices", os.path.basename(file).replace(".json", ""))
         )
+
         # png生成コマンド実行
         subprocess.run(
             args=["si-wrapper", "gerber2png"],
             cwd=os.path.join("slices", os.path.basename(file).replace(".json", ""))
         )
+
         # pnp生成コマンド実行
         subprocess.run(
             args=["kmake", "pnp", "-t"],
             cwd=os.path.join("slices", os.path.basename(file).replace(".json", ""))
         )
+
         # stackup生成コマンド実行
         subprocess.run(
             args=["kmake", "stackup-export"],
             cwd=os.path.join("slices", os.path.basename(file).replace(".json", ""))
         )
+
         # fabディレクトリ内のcsvファイルを処理
-        for file in glob.glob(os.path.join("slices", os.path.basename(file).replace(".json", ""), "fab", "*.csv")):
+        for csv_file in glob.glob(os.path.join("slices", os.path.basename(file).replace(".json", ""), "fab", "*.csv")):
             write_data = []
-            shutil.copy(file, file + ".bak")  # バックアップ作成
-            with open(file, "r") as fp:
+            with open(csv_file, "r") as fp:
                 reader = csv.reader(fp)
                 write_data.append(next(reader))  # ヘッダー行
                 for row in reader:
                     if row[0].startswith("SP"):  # "SP"で始まる行のみ抽出
                         write_data.append(row)
-            with open(file, "w") as fp:
+            with open(csv_file, "w") as fp:
                 writer = csv.writer(fp)
                 writer.writerows(write_data)
-        sleep(1)  # 1秒待機
+
+        # netinfo.jsonは使わないのでリネーム
+        os.rename(
+            os.path.join("slices", os.path.basename(file).replace(".json", ""), "netinfo.json"),
+            os.path.join("slices", os.path.basename(file).replace(".json", ""), "netinfo.json.bak")
+        )
+
+        # シミュレーション設定ファイルの編集
+        edit_simulation_settings(
+            os.path.join("slices", os.path.basename(file).replace(".json", ""), "simulation.json"),
+            frequency__start=1e9,
+            frequency__stop=20e9,
+            pixel_size=10,
+            max_steps=8096,
+            via__filling_epsilon=1,
+            via__plating_thickness=50,
+            mesh__xy=50,
+            mesh__inter_layers=4,
+            mesh__margin__xy=200,
+            mesh__margin__z=200,
+            margin__xy=500,
+            margin__z=500
+        )
+
+        # 1秒待機
+        sleep(1)
 
 
 if __name__ == "__main__":
